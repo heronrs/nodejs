555b0ad5a33919ed22dea9fc9d27e71a5a87dd36
Heron Rossi <rossiheron@gmail.com>
2022-01-10 14:18:58 -0300

    env variable test

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-const http = require('http');
+const http = require("http");
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Test Hello Deploy from Convox!\nI\'m: ' + process.env.APP + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK);
-  console.log('PING');
+  res.setHeader("Content-Type", "text/plain");
+  res.end(
+    "Test Hello Deploy from Convox!\nI'm: " +
+      process.env.APP +
+      "\nBuild:" +
+      process.env.BUILD +
+      "\nRelease: " +
+      process.env.RELEASE +
+      "\nrunning on: " +
+      process.env.RACK +
+      "\n MYVAR " +
+      process.env.MYVAR
+  );
 });
 
 server.listen(port, () => {
