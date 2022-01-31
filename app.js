01e582d89a9153cbaf91412b03517be32bf52924
Heron Rossi <rossiheron@gmail.com>
2022-01-31 13:38:41 -0300

    New test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Deploy from Convox!\nI\'m: ' + process.env.APP + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK);
+  res.end('Hello Demo Deploy from Convox!\nI\'m: ' + process.env.APP + '\nBuild:' + process.env.BUILD + '\nRelease: ' + process.env.RELEASE + '\nrunning on: ' + process.env.RACK);
   console.log('PING');
 });
 
